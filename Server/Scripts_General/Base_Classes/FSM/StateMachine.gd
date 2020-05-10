bb97f9c6c029857fa681b2fedd1b3c79ead371a7
Evan Cheng <dr.frolicks@gmail.com>
2020-05-09 20:19:50 -0700

    commented buggy fsm stuff

--- Server/Scripts_General/Base_Classes/FSM/StateMachine.gd
+++ Server/Scripts_General/Base_Classes/FSM/StateMachine.gd
@@ -52,6 +52,6 @@ func _unhandled_key_input(event):
 	if state.has_method("unhandled_key_input"):
 		state.unhandled_key_input(event)
 
-func _notification(what):
-	if state && state.has_method("notification"):
-		state.notification(what)
+#func _notification(what):
+#	if state && state.has_method("notification"):
+#		state.notification(what)
