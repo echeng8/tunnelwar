56549722003019baa3ab4496c37ba50645b332df
Evan Cheng <dr.frolicks@gmail.com>
2020-09-14 21:27:56 -0700

    7 players supported

--- Server/UI/Leaderboard.gd
+++ Server/UI/Leaderboard.gd
@@ -15,6 +15,7 @@ remote func initialize_rpc_sender():
 	rpc_id(get_tree().get_rpc_sender_id(), "set_rankings", player_rankings) 
 
 func update_rankings(_id = -1): #dummy arg to get signals to work
+	return #todo rewrite this
 	player_rankings = [] 
 	for child in gamestate.world_node.get_node("Players").get_children():
 		player_rankings.append(child.name)
